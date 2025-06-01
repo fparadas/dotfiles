4778d72763aafc34514a7fe5402762ebe33d1172
fparadas <felipegparadas@gmail.com>
2025-06-01 10:09:46 -0300

    added global ignore to .nix-mix

--- config/git.nix
+++ config/git.nix
@@ -21,7 +21,7 @@
       fzf = "!git checkout $(git branch --color=always --all --sort=-committerdate | grep -v HEAD | fzf --height 50% --ansi --no-multi --preview-window right:65%  --preview 'git log -n 50 --color=always --date=short --pretty=\"format:%C(auto)%cd %h%d %s\" $(sed \"s/.* //\" <<< {})' | sed \"s/.* //\")";
     };
 
-    ignores = [ ".DS_Store" ];
+    ignores = [ ".DS_Store", ".nix-mix/" ];
 
     extraConfig = {
       init = {
